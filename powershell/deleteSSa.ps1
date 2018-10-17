44137992cf3b56d2fb365d95fa1533e978862698
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-10-16 21:24:44 -0300

    Update deleteSSa.ps1
    Add alternative Solution

--- powershell/deleteSSa.ps1
+++ powershell/deleteSSa.ps1
@@ -1,3 +1,5 @@
 $ssa = Get-SPEnterpriseSearchServiceApplication -id “<GUID of your Search>”
 $ssa.unprovision(1)
 $ssa.Delete() 
+$spapp = Get-SPServiceApplication -Name "Contoso BDC Service"
+Remove-SPServiceApplication $spapp -RemoveData

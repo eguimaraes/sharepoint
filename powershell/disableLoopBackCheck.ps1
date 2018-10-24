6dd3e5948ec46789e080b92862398dccbb3a9cea
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-10-24 16:06:54 -0300

    Update disableLoopBackCheck.ps1
    Adicionado Link para o site - https://blogs.technet.microsoft.com/sharepoint_foxhole/2010/06/21/disableloopbackcheck-lets-do-it-the-right-way/

--- powershell/disableLoopBackCheck.ps1
+++ powershell/disableLoopBackCheck.ps1
@@ -1,1 +1,2 @@
+#Saiba mais em https://blogs.technet.microsoft.com/sharepoint_foxhole/2010/06/21/disableloopbackcheck-lets-do-it-the-right-way/
 New-ItemProperty HKLM:\System\CurrentControlSet\Control\Lsa -Name "DisableLoopbackCheck" -Value "1" -PropertyType dword

3167d13967c950f34c53f063167a8bdc8b7e3494
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-13 21:05:43 -0200

    Create DeployDisableLoopBackCheck.ps1

---
+++ DeployDisableLoopBackCheck.ps1
@@ -0,0 +1,3 @@
+#Saiba mais em https://blogs.technet.microsoft.com/sharepoint_foxhole/2010/06/21/disableloopbackcheck-lets-do-it-the-right-way/
+#disableLoopBackCheck.ps1
+New-ItemProperty HKLM:\System\CurrentControlSet\Control\Lsa -Name "DisableLoopbackCheck" -Value "1" -PropertyType dword

d84b9aa388376a8d0286d5981cb0df9a6d48caaf
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-13 20:51:10 -0200

    Update InstallPreReqOffline.ps1

--- InstallPreReqOffline.ps1
+++ InstallPreReqOffline.ps1
@@ -10,9 +10,9 @@ Start-Process "$PreRequsInstallerPath\PrerequisiteInstaller.exe" -Wait -Argument
               /Sync:`"$PreRequsFilesPath\Synchronization.msi`" `                                                                                  
               /AppFabric:`"$PreRequsFilesPath\WindowsServerAppFabricSetup_x64.exe`" ` 
               /kb3092423:`"$PreRequsFilesPath\AppFabric-KB3092423-x64-ENU.exe`" ` 
-              /MSIPCClient:`"$PreRequsFilesPath\setup_msipc_x64.msi`" ` 
+              /MSIPCClient:`"$PreRequsFilesPath\setup_msipc_x64.exe`" ` 
               /wcfdataservices56:`"$PreRequsFilesPath\WcfDataServices.exe`" ` 
               /odbc:`"$PreRequsFilesPath\msodbcsql.msi`" ` 
               /msvcrt11:`"$PreRequsFilesPath\vc_redist.x64.exe`" ` 
               /msvcrt14:`"$PreRequsFilesPath\vcredist_x64.exe`" ` 
-              /dotnetfx:`"$PreRequsFilesPath\NDP46-KB3045557-x86-x64-AllOS-ENU.exe`""
+              /dotnetfx:`"$PreRequsFilesPath\NDP452-KB2901907-x86-x64-AllOS-ENU.exe`""

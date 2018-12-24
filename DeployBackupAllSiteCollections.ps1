99efe244876540df9b8f318a96766ad331731b3c
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-24 20:53:38 -0200

    Update DeployBackupAllSiteCollections.ps1

--- DeployBackupAllSiteCollections.ps1
+++ DeployBackupAllSiteCollections.ps1
@@ -1,10 +1,11 @@
 $url=read-host "Digite a URL da WebApplication: ";
+$prefixo=read-host "Digite o prefixo do Path (Ex. D:\): ";
 $sufixo=read-host "Digite o managed path da SiteCollection: ";
 $app=Get-SPWebApplication $url
 $sites=$app.sites
 foreach ($site in $sites){
-$path="d:\"+$site.url.replace($url,"").replace($sufixo,"");
-if($path -eq "d:\"){$path="d:\"+$site.RootWeb.title}
+$path=prefixo+$site.url.replace($url,"").replace($sufixo,"");
+if($path -eq prefixo ){$path=prefixo+$site.RootWeb.title}
 $path=$path+".bak";
 export-spweb -Identity $url -path $path -Force
 }

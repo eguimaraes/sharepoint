34228a36e88cd3c3e7a28c2ff1f7d597dbbead3d
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-24 17:41:52 -0200

    Update DeployBackupAllSiteCollections.ps1

--- DeployBackupAllSiteCollections.ps1
+++ DeployBackupAllSiteCollections.ps1
@@ -1,5 +1,4 @@
-write-host("Digite a URL da WebApplication: ");
-$url=read-host();
+$url=read-host "Digite a URL da WebApplication: ";
 $app=Get-SPWebApplication $url
 $sites=$app.sites
 foreach ($site in $sites){

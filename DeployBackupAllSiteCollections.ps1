e6bb3c9db5bac4964b7cb5760fe15077f8fe01ae
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-24 20:43:07 -0200

    Update DeployBackupAllSiteCollections.ps1

--- DeployBackupAllSiteCollections.ps1
+++ DeployBackupAllSiteCollections.ps1
@@ -3,7 +3,9 @@ $sufixo=read-host "Digite o managed path da SiteCollection: ";
 $app=Get-SPWebApplication $url
 $sites=$app.sites
 foreach ($site in $sites){
-$path="d:\"+$site.url.replace($url+$sufixo,"");
-$path=($path.length>0)?$path:$site.title
+$path="d:\\"+$site.url.replace($url+$sufixo,"");
+if($path.length==0 -AND $path -eq "d:\\"){"d:\"+$site.RootWeb.title}
+write-host $path
+read-host
 export-spweb -Identity $url -path $path -Force
 }

2a5e1a65d7c3705c6e5c2868c90dbdae912a6592
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-24 17:43:17 -0200

    Update DeployBackupAllSiteCollections.ps1

--- DeployBackupAllSiteCollections.ps1
+++ DeployBackupAllSiteCollections.ps1
@@ -2,6 +2,6 @@ $url=read-host "Digite a URL da WebApplication: ";
 $app=Get-SPWebApplication $url
 $sites=$app.sites
 foreach ($site in $sites){
-$path="d:\"+$site.RootWeb.title;
-export-spweb -Identity $site.url.replace($url,""); -path $path -Force
+$path="d:\"+$site.url.replace($url,"");
+export-spweb -Identity $url; -path $path -Force
 }

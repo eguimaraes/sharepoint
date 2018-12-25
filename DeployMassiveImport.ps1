fb6b71eb87d9b30f89aff6b83932c69ec74ccb73
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-24 23:29:50 -0200

    Update DeployMassiveImport.ps1

--- DeployMassiveImport.ps1
+++ DeployMassiveImport.ps1
@@ -9,15 +9,16 @@ $sites="site1","site0","site2","site13";
 foreach($site in $sites){
 
 $url=$urlBase+"/sites/"+$site;
-   
-write-host("Creating Content DB");
 
-$cdb="WSS_Content_"+$site;
-
-New-SPContentDatabase $cdb -WebApplication $urlBase
+$cdb="WSS_Content_"
 
 write-host("Creating SiteCollections");
 
 New-SPSite $url -ContentDatabase $cdb -OwnerAlias "domain\user";
+
+$path=$site+".bak";
+
+Import-SPWeb $url -Path $path -UpdateVersions Overwrite
+
 
 }

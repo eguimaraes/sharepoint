54a2273c590179f069ba681a3979f4847110619e
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-26 09:51:16 -0200

    Update DeployRemoveFeatures.ps1

--- DeployRemoveFeatures.ps1
+++ DeployRemoveFeatures.ps1
@@ -2,8 +2,20 @@ $url=read-host "Digite a URL da WebApplication: "
 $featureName=read-host "Digite o Nome da Feature: "
 $features=Get-SPFeature | Where-Object { $_.DisplayName -like $featureName+"*" }
 foreach($feature in $features){
+Disable-SPFeature –identity $feature.DisplayName -URL $url -confirm:$false -force
 
-Disable-SPFeature –identity $feature.DisplayName -URL $url
+$sites=get-spsite -Limit all;
+
+foreach($site in $sites){
+
+write-host $site.RootWeb.url;
+ 
+write-host $feature.DisplayName
+
+Disable-SPFeature –identity $feature.DisplayName -URL $site.RootWeb.url -confirm:$false -force
+
+
+}
 
 
 

b38fcfccb6268bad64c87de8696659e50fd3b540
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-26 06:50:12 -0200

    Update DeployIdentityFeatures.ps1

--- DeployIdentityFeatures.ps1
+++ DeployIdentityFeatures.ps1
@@ -1,4 +1,4 @@
 $url=read-host "Digite a URL da WebApplication: "
 $feature=read-host "Digite o Nome da Feature: "
-Get-SPFeature | Where-Object { $_.DisplayName -like "Excel*" }
+Get-SPFeature | Where-Object { $_.DisplayName -like $feature+"*" }
 

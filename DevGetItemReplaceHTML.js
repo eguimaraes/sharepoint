970916d3596af66637b7ec0ca9e956034bd35c1b
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-31 12:26:31 -0200

    Create DevGetItemReplaceHTML.js

---
+++ DevGetItemReplaceHTML.js
@@ -0,0 +1,34 @@
+function GetItem(list,title,field,obj){
+var retorno  
+var siteUrl=_spPageContextInfo.webAbsoluteUrl;
+var clientContext = new SP.ClientContext(siteUrl);
+var web = clientContext.get_web();
+var oList = clientContext.get_web().get_lists().getByTitle(list);
+var camlQuery = new SP.CamlQuery();
+  camlQuery.set_viewXml('<View><Query>Where><Eq><FieldRef Name=\"Title\" /><Value Type=\"Text\">"+title+"</Value></Eq></Where></Query><RowLimit>10</RowLimit></View>');
+  var collListItem = oList.getItems(camlQuery);
+clientContext.load(web); 
+clientContext.load(oList);
+  clientContext.load(collListItem);
+  
+clientContext.executeQueryAsync(
+function () {
+
+var listItemEnumerator = collListItem.getEnumerator();        
+    while (listItemEnumerator.moveNext()) {
+        var oListItem = listItemEnumerator.get_current();
+      
+         document.getElementById(obj).innerHTML=oListItem.get_item(field);
+          
+      
+           break; 
+    }
+        },
+
+                    function () {alert('Erro') } 
+  
+  
+  
+  )
+
+}

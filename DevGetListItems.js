5bea9eb70c4734a349bbc8437073188fac1cc36c
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2018-12-31 11:48:44 -0200

    Update DevGetListItems.js

--- DevGetListItems.js
+++ DevGetListItems.js
@@ -1,18 +1,33 @@
-function GetItem(title){
+function GetItem(list,title,field){
+var retorno  
 var siteUrl=_spPageContextInfo.webAbsoluteUrl;
 var clientContext = new SP.ClientContext(siteUrl);
 var web = clientContext.get_web();
-var oList = clientContext.get_web().get_lists().getByTitle("ListName");
+var oList = clientContext.get_web().get_lists().getByTitle(list);
+var camlQuery = new SP.CamlQuery();
+  camlQuery.set_viewXml('<View><Query>Where><Eq><FieldRef Name=\"Title\" /><Value Type=\"Text\">"+title+"</Value></Eq></Where></Query><RowLimit>10</RowLimit></View>');
+var collListItem = oList.getItems(camlQuery);
 clientContext.load(web); 
 clientContext.load(oList);
+  clientContext.load(collListItem);
+  
 clientContext.executeQueryAsync(
 function () {
 
-                        location.href = web.get_url()+"URLFinal";
+var listItemEnumerator = collListItem.getEnumerator();        
+    while (listItemEnumerator.moveNext()) {
+        var oListItem = listItemEnumerator.get_current();
+      
+           retorno= oListItem.get_item(field);
+            
+    }
+
                     },
 
-                    function () {alert('Erro')
-                    
-                    } 
-varItem=
+                    function () {alert('Erro') } 
+  
+  
+  
+  })
+return retorno
 }

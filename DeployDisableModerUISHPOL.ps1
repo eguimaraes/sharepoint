8f11ad1f0910d9b506dc065f0ca67e0061626571
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-03-08 10:17:40 -0300

    Create DeployDisableModerUISHPOL.ps1

---
+++ DeployDisableModerUISHPOL.ps1
@@ -0,0 +1,8 @@
+# Connect to a site
+$cred = Get-Credential
+Connect-PnPOnline -Url https://[tenant].sharepoint.com/sites/siteurl -Credentials $cred
+
+# Prevent site pages at web level
+Disable-PnPFeature -Identity B6917CB1-93A0-4B97-A84D-7CF49975D4EC -Scope Web 
+# And again enable site pages at web
+#Enable-PnPFeature -Identity B6917CB1-93A0-4B97-A84D-7CF49975D4EC -Scope Web 

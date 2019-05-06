99b516f3e05f03a7f1dfc779b8dba3f9115373e0
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-05-06 08:21:53 -0300

    Create sharefolder.bat

---
+++ sharefolder.bat
@@ -0,0 +1,5 @@
+Http: <servidor-sharepoint>davwwwroot<site><biblioteca> </biblioteca> </site> </servidor-sharepoint>
+HTTPS (SSL): \<servidor-sharepoint>@ssl davwwwroot<site><biblioteca> </biblioteca> </site> </servidor-sharepoint>
+The part of "davwwwroot" is mandatory. In our example, then, the UNC path to access SharePoint from the command line would look like this:
+
+\lambda31.sharepoint.com @ ssldavwwwrootdocumentos%20compartilhados

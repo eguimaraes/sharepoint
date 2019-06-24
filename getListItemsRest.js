c3e4704355af8f60ba550fb6a658eea8ef51ce38
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-06-24 19:55:21 -0300

    Create getListItemsRest.js

---
+++ getListItemsRest.js
@@ -0,0 +1,13 @@
+function getListItems(listName, siteurl, success, failure) {
+    $.ajax({
+        url: siteurl + "/_api/web/lists/getbytitle('" + listName + "')/items",
+        method: "GET",
+        headers: { "Accept": "application/json; odata=verbose" },
+        success: function (data) {
+            success(data);
+        },
+        error: function (data) {
+            failure(data);
+        }
+    });
+}

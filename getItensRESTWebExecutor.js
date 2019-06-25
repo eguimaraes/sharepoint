eafa478deadec8f1dc962a5916991529b54f5363
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-06-24 21:56:11 -0300

    Create getItensRESTWebExecutor.js

---
+++ getItensRESTWebExecutor.js
@@ -0,0 +1,36 @@
+var hostweburl;
+var appweburl;
+
+// Get the URLs for the add-in web the host web URL from the query string.
+$(document).ready(function () {
+  //Get the URI decoded URLs.
+  hostweburl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+  appweburl = decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+
+  // Load the SP.RequestExecutor.js file.
+  $.getScript(hostweburl + "/_layouts/15/SP.RequestExecutor.js", runCrossDomainRequest);
+});
+
+// Build and send the HTTP request.
+function runCrossDomainRequest() {
+  var executor = new SP.RequestExecutor(appweburl); 
+  executor.executeAsync({
+      url: appweburl + "/_api/SP.AppContextSite(@target)/web/lists?@target='" + hostweburl + "'",
+      method: "GET", 
+      headers: { "Accept": "application/json; odata=verbose" }, 
+      success: successHandler, 
+      error: errorHandler 
+  });
+}
+
+// Get a query string value.
+// For production add-ins, you may want to use a library to handle the query string.
+function getQueryStringParameter(paramToRetrieve) {
+  var params = document.URL.split("?")[1].split("&amp;");
+  var strParams = "";
+  for (var i = 0; i < params.length; i = i + 1) {
+    var singleParam = params[i].split("=");
+    if (singleParam[0] == paramToRetrieve) return singleParam[1];
+  }
+}
+??? // success and error callback functions

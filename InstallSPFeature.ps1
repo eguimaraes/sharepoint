3882a786607a533009495f908bfc8014d23e4e20
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-06-26 17:11:06 -0300

    Create InstallSPFeature.ps1

---
+++ InstallSPFeature.ps1
@@ -0,0 +1,41 @@
+Add-SPSolution
+   [-LiteralPath] <String>
+   [-AssignmentCollection <SPAssignmentCollection>]
+   [-Confirm]
+   [-Language <UInt32>]
+   [-WhatIf]
+   [<CommonParameters>]
+   Add-SPSolution -LiteralPath c:\contoso_solution.wsp
+   
+   
+
+Install-SPSolution
+       [-Identity] <SPSolutionPipeBind>
+       [-AllWebApplications]
+       [-AssignmentCollection <SPAssignmentCollection>]
+       [-CASPolicies]
+       [-CompatibilityLevel <String>]
+       [-Confirm]
+       [-Force]
+       [-FullTrustBinDeployment]
+       [-GACDeployment]
+       [-Language <UInt32>]
+       [-Local]
+       [-Time <String>]
+       [-WebApplication <SPWebApplicationPipeBind>]
+       [-WhatIf]
+       [<CommonParameters>]
+
+Install-SPSolution -Identity contoso_solution.wsp -GACDeployment
+
+Install-SPSolution -Identity contoso_solution.wsp -GACDeployment -CompatibilityLevel {14,15}
+
+Install-SPFeature
+       [-Path] <String>
+       [-AssignmentCollection <SPAssignmentCollection>]
+       [-CompatibilityLevel <Int32>]
+       [-Confirm]
+       [-Force]
+       [-WhatIf]
+       [<CommonParameters>]
+Install-SPFeature -path "MyCustomFeature"

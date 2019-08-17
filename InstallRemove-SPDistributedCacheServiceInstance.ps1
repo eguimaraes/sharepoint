a79066d252d714b0003968adf86cbf1ef6f2e58d
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-08-16 21:14:08 -0300

    Update InstallRemove-SPDistributedCacheServiceInstance.ps1

--- InstallRemove-SPDistributedCacheServiceInstance.ps1
+++ InstallRemove-SPDistributedCacheServiceInstance.ps1
@@ -1,1 +1,8 @@
 Remove-SPDistributedCacheServiceInstance
+$SPFarm = Get-SPFarm
+ $cacheClusterName = "SPDistributedCacheCluster_" + $SPFarm.Id.ToString()
+ $cacheClusterManager = [Microsoft.SharePoint.DistributedCaching.Utilities.SPDistributedCacheClusterInfoManager]::Local
+ $cacheClusterInfo = $cacheClusterManager.GetSPDistributedCacheClusterInfo($cacheClusterName);
+ $instanceName ="SPDistributedCacheService Name=AppFabricCachingService"
+ $serviceInstance = Get-SPServiceInstance | ? {($_.Service.Tostring()) -eq $instanceName -and ($_.Server.Name) -eq $env:computername}
+ $serviceInstance.Delete()

1f85c3deeed277594ab83e9b7731fd1bdaf7b5e2
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-08-21 15:02:24 -0300

    Update SPDistributedCacheServiceInstance.ps1

--- SPDistributedCacheServiceInstance.ps1
+++ SPDistributedCacheServiceInstance.ps1
@@ -1,17 +1,14 @@
 Export-CacheClusterConfig -Path $PWD\AF.XML
-
-
-
+#verifique o arquivo para localizarr o registro do cache
+Unregister-CacheHost -HostName SP15-WFE.cloudnet.local -ProviderType SPDistributedCacheClusterProvider -ConnectionString \\NOMEsERVER
 $SPFarm = Get-SPFarm
 $cacheClusterName = "SPDistributedCacheCluster_" + $SPFarm.Id.ToString()
+
 $cacheClusterManager = [Microsoft.SharePoint.DistributedCaching.Utilities.SPDistributedCacheClusterInfoManager]::Local
 $cacheClusterInfo = $cacheClusterManager.GetSPDistributedCacheClusterInfo($cacheClusterName);
 $instanceName ="SPDistributedCacheService Name=AppFabricCachingService"
 $serviceInstance = Get-SPServiceInstance | ? {($_.Service.Tostring()) -eq $instanceName -and ($_.Server.Name) -eq $env:computername}
 $serviceInstance.Delete()
-
-
-Unregister-CacheHost -HostName SP15-WFE.cloudnet.local -ProviderType SPDistributedCacheClusterProvider -ConnectionString \\NOMEsERVER
 
 Remove-SPDistributedCacheServiceInstance
 Add-SPDistributedCacheServiceInstance -Role WebFrontEndWithDistributedCache

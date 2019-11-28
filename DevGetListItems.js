d245ab1c4cfb624b5a3871e21e6cb91045c3e2cf
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-11-28 20:30:50 -0300

    Update DevGetListItems.js

--- DevGetListItems.js
+++ DevGetListItems.js
@@ -5,7 +5,7 @@ var clientContext = new SP.ClientContext(siteUrl);
 var web = clientContext.get_web();
 var oList = clientContext.get_web().get_lists().getByTitle(list);
 var camlQuery = new SP.CamlQuery();
-  camlQuery.set_viewXml('<View><Query>Where><Eq><FieldRef Name=\"Title\" /><Value Type=\"Text\">"+title+"</Value></Eq></Where></Query><RowLimit>10</RowLimit></View>');
+  camlQuery.set_viewXml("<View><Query>Where><Eq><FieldRef Name=\"Title\" /><Value Type=\"Text\">"+title+"</Value></Eq></Where></Query><RowLimit>10</RowLimit></View>");
 alert(camlQuery.text);
   var collListItem = oList.getItems(camlQuery);
 clientContext.load(web); 

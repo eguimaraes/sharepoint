f0323d9323a57b41f2d3d7299881c9562f2735cb
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2020-08-05 17:39:59 -0300

    Create installDepSPfx.bat

---
+++ installDepSPfx.bat
@@ -0,0 +1,6 @@
+rem Install the latest version of Node.js LTS 10.x.
+npm install gulp yo @microsoft/generator-sharepoint –global
+npm install gulp –global
+npm install yo –global
+npm install @microsoft/generator-sharepoint –global
+gulp trust-dev-cert

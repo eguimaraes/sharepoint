33b2eceb8e60b326393c68ba50c22d22b6412f75
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2020-11-15 22:07:56 -0300

    Create addNewWebPart.ps1

---
+++ addNewWebPart.ps1
@@ -0,0 +1,29 @@
+npm install gulp yo @microsoft/generator-sharepoint --global
+npm install gulp --global
+npm install yo --global
+npm install @microsoft/generator-sharepoint --global
+yo @microsoft/sharepoint
+npm install jquery@2 --save
+npm install jqueryui --save
+npm install @types/jquery@2 --save
+npm install @types/jqueryui --save
+npm install @types/microsoft-ajax @types/sharepoint --save-dev
+npm install @types/angular --save
+npm install @microsoft/sp-core-library
+npm install moment --save
+npm install angular --save
+npm install @pnp/logging @pnp/common @pnp/odata @pnp/sp --save
+npm install @pnp/sp @pnp/graph --save
+npm install jquery --save
+npm install @types/jquery --save-dev
+npm install bootstrap --save  --save-dev
+npm install @types/bootstrap@4 --save-dev
+npm install --save @fortawesome/fontawesome-free  --save
+npm install url-loader --save-dev
+npm install @fluentui/react --save
+npm install @types/react  --save
+npm install react --save
+npm install @pnp/spfx-controls-react --save --save-exact
+npm install @pnp/spfx-property-controls --save --save-exact
+npm install @pnp/logging @pnp/common @pnp/odata @pnp/sp @pnp/graph --save
+npm install  @pnp/adaljsclient @pnp/msaljsclient --save

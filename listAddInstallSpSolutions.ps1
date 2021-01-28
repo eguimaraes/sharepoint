dad06d9f9c31d71bc31158e054b40b6005c552d7
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-01-28 17:03:07 -0300

    Update listAddInstallSpSolutions.ps1

--- listAddInstallSpSolutions.ps1
+++ listAddInstallSpSolutions.ps1
@@ -1,5 +1,5 @@
 $arquivos=ls *.wsp
 foreach ($arquivo in $arquivos){
-Add-SPSolution -LiteralPath  $PWD.tostring()+"\"+$arquivo.name.tostring()
-Install-SPSolution -Identity $arquivo.name.tostring() GACDeployment -AllWebApplications
+Add-SPSolution -LiteralPath  $arquivo.FullName+"\"+$arquivo.name.tostring()
+Install-SPSolution -Identity $arquivo.name -GACDeployment -AllWebApplications
 }

4d4d5d691a940588a552e3449661f72a33fb04ef
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-02-08 17:46:09 -0300

    Create fixManagedMetadataAssociation.ps1

---
+++ fixManagedMetadataAssociation.ps1
@@ -0,0 +1,33 @@
+function FixNavigation($url)
+{
+    try
+    {
+        $web = Get-SPWeb $url;
+        Write-Host ([String]::Format("Procesing web {0}",$web.Url)) -foregroundcolor White
+        $WebNavSettings = New-Object Microsoft.SharePoint.Publishing.Navigation.WebNavigationSettings($Web);
+        $WebNavSettings.GlobalNavigation.Source = "PortalProvider"
+        $WebNavSettings.CurrentNavigation.Source = "PortalProvider"
+        $WebNavSettings.Update()
+        Write-Host ([String]::Format("Updated navigation to Structured {0}",$web.Url)) -foregroundcolor blue
+        $web = Get-SPWeb $url;
+        $WebNavSettings = New-Object Microsoft.SharePoint.Publishing.Navigation.WebNavigationSettings($Web);
+        $WebNavSettings.GlobalNavigation.Source = "TaxonomyProvider"
+        $WebNavSettings.CurrentNavigation.Source = "TaxonomyProvider"
+        $WebNavSettings.Update()
+        Write-Host ([String]::Format("Updated navigation to Structured {0}",$web.Url)) -foregroundcolor green
+        if($web.Webs.Count -gt 0)
+        {
+            foreach($subWeb in $web.Webs)
+           {
+                #$web.Url
+                FixNavigation $subWeb.Url;
+            }
+        }
+    }
+    catch
+    {
+        Write-Host ([String]::Format("Error processing web at $url, with Exception: {0}",   $_.Exception.Message)) -foregroundcolor Red
+    }
+}
+$url = "https://a/"
+FixNavigation($url)

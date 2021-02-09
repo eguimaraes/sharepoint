c75247f12a5e4b2c47656e22f28c26e202337ec7
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-02-08 23:29:38 -0300

    Create listDeployMultiples.ps1

---
+++ listDeployMultiples.ps1
@@ -0,0 +1,7 @@
+$webapps=@("webappurl","webappur2")
+foreach ($webapp in $webapps){
+Get-ChildItem | ForEach-Object{Add-SPSolution -LiteralPath $_.Fullname};
+Get-SPSolution | ForEach-Object {If ($_.ContainsWebApplicationResource -eq $False)
+{Install-SPSolution -Identity $_ -GACDeployment} else {Install-SPSolution -Identity $_ -WebApplication $webapp -GACDeployment}}
+Read-Host "Continuar?"
+}

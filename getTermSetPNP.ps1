2823787ababe52d04eae3cc8168c203a51bb7076
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-02-10 10:55:58 -0300

    Create getTermSetPNP.ps1

---
+++ getTermSetPNP.ps1
@@ -0,0 +1,3 @@
+Get-PnPTermSet -TermGroup "Corporate"
+Get-PnPTermSet -Identity "Departments" -TermGroup "Corporate"
+Get-PnPTermSet -Identity ab2af486-e097-4b4a-9444-527b251f1f8d -TermGroup "Corporate

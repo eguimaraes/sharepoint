003286ed4c2ab7bb5dfeb5f8edc547accf640b01
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-02-21 11:56:10 -0300

    Create UsoArroba.cs

---
+++ UsoArroba.cs
@@ -0,0 +1,11 @@
+#https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/verbatim
+string[] @for = { "John", "James", "Joan", "Jamie" };
+for (int ctr = 0; ctr < @for.Length; ctr++)
+{
+   Console.WriteLine($"Here is your gift, {@for[ctr]}!");
+}
+// The example displays the following output:
+//     Here is your gift, John!
+//     Here is your gift, James!
+//     Here is your gift, Joan!
+//     Here is your gift, Jamie!

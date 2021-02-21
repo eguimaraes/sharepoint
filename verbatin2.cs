733ce6df62025eb7cedf3560fc6c29e4e2eb2916
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-02-21 11:57:00 -0300

    Create verbatin2.cs

---
+++ verbatin2.cs
@@ -0,0 +1,8 @@
+string filename1 = @"c:\documents\files\u0066.txt";
+string filename2 = "c:\\documents\\files\\u0066.txt";
+
+Console.WriteLine(filename1);
+Console.WriteLine(filename2);
+// The example displays the following output:
+//     c:\documents\files\u0066.txt
+//     c:\documents\files\u0066.txt

72566c8535689435ce7eab8ed48c9eefc0562e0d
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-02-25 09:44:59 -0300

    Create ExGet-SPServiceApplication.ps1

---
+++ ExGet-SPServiceApplication.ps1
@@ -0,0 +1,4 @@
+Get-SPServiceApplication
+Get-SPServiceApplication -Identity e2c2be70-6382-4ce7-8a44-ae7dadff5597
+Get-SPServiceApplication -Identity e2c2be70-6382-4ce7-8a44-ae7dadff5597
+Get-SPServiceApplication -Name AccountingServiceApp

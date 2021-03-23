998df4dfc57eba7675291bcd1eeb5c751e4ca26e
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-03-23 02:30:58 -0300

    Create spfxPublicarwp.ps1

---
+++ spfxPublicarwp.ps1
@@ -0,0 +1,9 @@
+gulp trust-dev-cert
+gulp build
+gulp bundle
+gulp --ship
+gulp build --ship
+gulp package-solution
+gulp package-solution --production
+gulp package-solution --ship
+gulp trust-dev-cert

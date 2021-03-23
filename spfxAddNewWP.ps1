a310cb476d9655d59c13be9fb80beb3f67b59f84
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-03-23 02:27:31 -0300

    Create spfxAddNewWP.ps1

---
+++ spfxAddNewWP.ps1
@@ -0,0 +1,34 @@
+npm install yo --global
+npm install gulp yo @microsoft/generator-sharepoint --global
+npm install -g @pnp/generator-spfx
+npm install gulp --global
+npm install @microsoft/generator-sharepoint
+npm install adaptivecards --save
+npm install react-native-adaptivecards --save
+npm install jquery@2 --save
+npm install jqueryui --save
+npm install @types/jquery@2 --save
+npm install @types/jqueryui --save
+npm install @types/microsoft-ajax @types/sharepoint --save-dev
+npm install @types/angular --save
+npm install @microsoft/sp-core-library
+npm install moment --save
+npm install angular --save
+npm install @pnp/logging @pnp/common @pnp/odata @pnp/sp --save
+npm install @pnp/sp @pnp/graph --save
+npm install jquery --save
+npm install @types/jquery --save-dev
+npm install bootstrap --save  --save-dev
+npm install @types/bootstrap@4 --save-dev
+npm install --save @fortawesome/fontawesome-free  --save
+npm install url-loader --save-dev
+npm install @fluentui/react --save
+npm install @types/react  --save
+npm install react --save
+npm install @pnp/spfx-controls-react --save --save-exact
+npm install @pnp/spfx-property-controls --save --save-exact
+npm install @pnp/logging @pnp/common @pnp/odata @pnp/sp @pnp/graph --save
+npm install @pnp/adaljsclient @pnp/msaljsclient --save
+npm install @microsoft/sp-build-web --save
+npm i --save @pnp/sp
+yo @pnp/spfx

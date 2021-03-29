d00622ffba795030c271bbfcd49290a4d4c8de71
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-03-29 00:50:34 -0300

    Create CreateAzureUsersCSV.ps1

---
+++ CreateAzureUsersCSV.ps1
@@ -0,0 +1,14 @@
+$invitations = import-csv c:\bulkinvite\invitations.csv
+
+$messageInfo = New-Object Microsoft.Open.MSGraph.Model.InvitedUserMessageInfo
+
+$messageInfo.customizedMessageBody = "Hello. You are invited to the Contoso organization."
+
+foreach ($email in $invitations)
+   {New-AzureADMSInvitation `
+      -InvitedUserEmailAddress $email.InvitedUserEmailAddress `
+      -InvitedUserDisplayName $email.Name `
+      -InviteRedirectUrl https://myapps.microsoft.com `
+      -InvitedUserMessageInfo $messageInfo `
+      -SendInvitationMessage $true
+   }

b45f83f07ca38d42e600fd191b9e4cd97e398bf1
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-03-29 00:41:50 -0300

    Create createNewADUser.ps1

---
+++ createNewADUser.ps1
@@ -0,0 +1,8 @@
+# Create a password object
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+
+# Assign the password
+$PasswordProfile.Password = "<Password>"
+
+# Create the new user
+New-AzureADUser -AccountEnabled $True -DisplayName "Abby Brown" -PasswordProfile $PasswordProfile -MailNickName "AbbyB" -UserPrincipalName "AbbyB@contoso.com"

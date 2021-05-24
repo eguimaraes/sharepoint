0c646bc38a6834020545407d50483848e1aeb6c0
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-05-24 13:34:48 -0300

    Create fqualifiedassembly.cs

---
+++ fqualifiedassembly.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+
+class asmname
+{
+    public static void Main()
+    {
+        Type t = typeof(System.Data.DataSet);
+        string s = t.Assembly.FullName.ToString();
+        Console.WriteLine("The fully qualified assembly name " +
+            "containing the specified class is {0}.", s);
+    }
+}

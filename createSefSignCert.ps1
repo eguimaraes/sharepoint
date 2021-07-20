e7704f29eb914fab1eada09dafbfcdf5333797c2
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-07-20 15:15:46 -0300

    Create createSefSignCert.ps1

---
+++ createSefSignCert.ps1
@@ -0,0 +1,1 @@
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature -Subject "CN=EDVALDO" -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign

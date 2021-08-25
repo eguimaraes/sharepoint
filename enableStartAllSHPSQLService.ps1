1fade0be5d3285a46b47781262094d708a8df45d
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-08-25 18:10:39 -0300

    Create enableStartAllSHPSQLService.ps1

---
+++ enableStartAllSHPSQLService.ps1
@@ -0,0 +1,16 @@
+$services=get-service;
+foreach($service in $services){
+
+if ($service.DisplayName.Contains("SharePoint") -or $service.DisplayName.Contains("SQL Serve") ){
+
+
+$continuar=read-host("Deseja Iniciar:"+$service.DisplayName+"?(s/n)")
+
+if($continuar -eq "s" -or $continuar -eq "S"){ 
+write-host("Habilitando e Iniciando" + $service.DisplayName)
+Set-Service $service.Name -startupType Manual
+Start-Service -Name $service.Name
+}
+
+}
+}

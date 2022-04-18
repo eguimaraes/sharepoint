771885bfe97de9733ea7281beae54ce680a2ff2b
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-04-18 03:49:06 -0300

    Create navigationTermSert.cs
    https://docs.microsoft.com/en-us/sharepoint/dev/general-development/how-to-use-code-to-pin-terms-to-navigation-term-sets-in-sharepoint

---
+++ navigationTermSert.cs
@@ -0,0 +1,87 @@
+#https://docs.microsoft.com/en-us/sharepoint/dev/general-development/how-to-use-code-to-pin-terms-to-navigation-term-sets-in-sharepoint
+public void TermPinningTest()
+        {
+using (SPSite site = new SPSite(TestConfig.ServerUrl))
+            {
+                using (SPWeb web = site.OpenWeb())
+                {
+                    TaxonomySession taxonomySession = new TaxonomySession(site, updateCache: true);
+
+                    // Create the navigation term set.
+                    NavigationTermSet menuNavTermSet = DemoUtilities.SetUpSampleNavTermSet(
+                        this.TestContext, taxonomySession, web);
+                    TermSet menuTaxTermSet = menuNavTermSet.GetTaxonomyTermSet();
+
+                    TermStore termStore = menuTaxTermSet.TermStore;
+                    Group group = menuTaxTermSet.Group;
+
+                    // Does the tagging Taxonomy term set already exist?
+                    TermSet taggingTaxTermSet = group.TermSets.FirstOrDefault(
+                        ts => ts.Id == TestConfig.TaggingTermSetId);
+
+                    if (taggingTaxTermSet != null)
+                    {
+                        Log("Deleting old tagging term set");
+
+                        // If the tagging Taxonomy term set already exists, delete the old one.
+                        taggingTaxTermSet.Delete();
+                        termStore.CommitAll();
+                    }
+
+                    Log("Creating the tagging term set");
+
+                    taggingTaxTermSet = group.CreateTermSet("Demo Tagging TermSet", TestConfig.TaggingTermSetId);
+
+                    int lcid = termStore.WorkingLanguage;
+
+                    // Create some terms.
+                    Term taggingProductsTaxTerm = taggingTaxTermSet.CreateTerm("Products", lcid);
+                    taggingProductsTaxTerm.CreateTerm("Electronics", lcid);
+                    taggingProductsTaxTerm.CreateTerm("Footwear", lcid);
+                    taggingProductsTaxTerm.CreateTerm("Sports", lcid);
+
+                    termStore.CommitAll();
+
+                    /// Pinning the products subtree. Pins the "Products" Term object to the NavigationTermSet object.
+                    Term menuProductsTaxTerm = menuTaxTermSet.ReuseTermWithPinning(taggingProductsTaxTerm);
+                    termStore.CommitAll();
+
+                    /// Creating the publishing page template DemoTargetPage.aspx");
+                    PublishingWeb publishingWeb = PublishingWeb.GetPublishingWeb(web);
+
+                    SPListItem pageListItem = null;
+                    PublishingPage publishingPage;
+                    try
+                    {
+                        pageListItem = web.GetListItem(web.Url + "/Pages/DemoTargetPage.aspx");
+                        publishingPage = PublishingPage.GetPublishingPage(pageListItem);
+   
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        Log("Creating new target page");
+                        publishingPage = publishingWeb.AddPublishingPage("DemoTargetPage.aspx", publishingWeb.DefaultPageLayout);
+                        Log("Checking in target page draft");
+                        publishingPage.CheckIn("TermPinningTest");
+                    }
+
+                    // Set a custom target page for the pinned terms and then set some navigation properties.
+
+                    // Normally the navigation objects are obtained by way of an optimized function such as
+                    // TaxonomyNavigation.GetTermSetForWeb() or TaxonomyNavigationContext.Current.NavigationTerm.
+                    // The function guarantees that URLs will be resolved using a valid NavigationTerm.View.
+                    // But because we are populating totally new data, the cache will probably not be updated
+                    // yet, so instead we manually construct a view using GetAsResolvedByWeb().
+                    NavigationTerm menuProductsNavTerm = NavigationTerm.GetAsResolvedByWeb(menuProductsTaxTerm,
+                        web, StandardNavigationProviderNames.GlobalNavigationTaxonomyProvider);
+
+                    menuProductsNavTerm.TargetUrl.Value = publishingPage.Uri.AbsolutePath;
+                    menuProductsNavTerm.TargetUrlForChildTerms.Value = publishingPage.Uri.AbsolutePath;
+
+                    termStore.CommitAll();
+
+                    Log("TermPinningTest completed successfully");
+                }
+            }
+
+}

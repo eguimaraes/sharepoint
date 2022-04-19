734524f122e1a93a457783756d0c97c2c8541d8f
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-04-19 11:01:33 -0300

    Create lendoUrmArquivoAteoFim.cs
    https://docs.microsoft.com/pt-br/dotnet/api/system.io.streamreader.peek?view=net-6.0

---
+++ lendoUrmArquivoAteoFim.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+
+class Test
+{
+    
+    #https://docs.microsoft.com/pt-br/dotnet/api/system.io.streamreader.peek?view=net-6.0
+    public static void Main()
+    {
+        string path = @"c:\temp\MyTest.txt";
+
+        try
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("This");
+                sw.WriteLine("is some text");
+                sw.WriteLine("to test");
+                sw.WriteLine("Reading");
+            }
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+
+                while (sr.Peek() > -1)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The process failed: {0}", e.ToString());
+        }
+    }
+}

2857090dfac4baafceead6a8c87a18a864b7748b
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-04-20 17:57:26 -0300

    Create createCertficate.ps1
    https://docs.microsoft.com/en-us/sharepoint/dev/solution-guidance/security-apponly-azuread

---
+++ createCertficate.ps1
@@ -0,0 +1,2 @@
+#https://docs.microsoft.com/en-us/sharepoint/dev/solution-guidance/security-apponly-azuread
+.\Create-SelfSignedCertificate.ps1 -CommonName "MyCompanyName" -StartDate 2017-10-01 -EndDate 2019-10-01

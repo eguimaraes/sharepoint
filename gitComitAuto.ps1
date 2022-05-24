279bcf528bb80991fd98a673dbd51108891f1874
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-05-24 12:36:21 -0300

    Create gitComitAuto.ps1

---
+++ gitComitAuto.ps1
@@ -0,0 +1,7 @@
+$msg=read-host("Digite a mensagem do commit")
+git add . --all;
+git commit -a -m $msg;
+git push;$msg=read-host("Digite a mensagem do commit")
+git add . --all;
+git commit -a -m $msg;
+git push;

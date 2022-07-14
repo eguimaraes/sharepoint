5a507cc860bec7a2f0edd14dc1e66b78d3c5eed2
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-07-14 14:31:52 -0300

    Create SetExecutarPowerShell.ps1
    https://docs.vmware.com/en/vRealize-Automation/7.6/com.vmware.vra.iaas.hp.doc/GUID-9670AFC5-76B8-4321-822A-BCE05800DB5B.html#:~:text=Select%20Start%20%3E%20All%20Programs%20%3E%20Windows,settings%20for%20the%20execution%20policy.

---
+++ SetExecutarPowerShell.ps1
@@ -0,0 +1,3 @@
+Set-ExecutionPolicy RemoteSigned to set the policy to RemoteSigned.
+Set-ExecutionPolicy Unrestricted to set the policy to Unrestricted.
+Get-ExecutionPolicy to verify the current settings for the execution policy.

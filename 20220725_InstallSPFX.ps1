d177239bcb0d447efb866b8fbf3c2c9f17441076
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-07-25 15:57:33 -0300

    Create 20220725_InstallSPFX.ps1
    https://docs.microsoft.com/en-us/sharepoint/dev/spfx/set-up-your-development-environment

---
+++ 20220725_InstallSPFX.ps1
@@ -0,0 +1,6 @@
+#https://docs.microsoft.com/en-us/sharepoint/dev/spfx/set-up-your-development-environment
+npm install gulp-cli yo @microsoft/generator-sharepoint --global
+npm install gulp-cli --global
+npm install yo --global
+npm install @microsoft/generator-sharepoint --global
+gulp trust-dev-cert

28e43f0b958059f0ba63bac25063fd09d2ce211a
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-07-28 22:17:52 -0300

    Create 20220728_installAllSol.ps1

---
+++ 20220728_installAllSol.ps1
@@ -0,0 +1,9 @@
+Add-PSSnapin Microsoft.SharePoint.PowerShell
+$webapps=read-host "Digite a URL do site"
+foreach($webapp in $webapps){
+$cont=read-host "Executando para ",$webapp,"continuar?(s/n)"
+if ($cont -eq "s") {
+Get-SPSolution | ForEach-Object {If ($_.ContainsWebApplicationResource -eq $False) {
+Install-SPSolution -Identity $_ -GACDeployment -Force} else {Install-SPSolution -Identity $_ -WebApplication $webapp -GACDeployment -Force}}
+}
+}

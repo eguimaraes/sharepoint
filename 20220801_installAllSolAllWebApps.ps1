20b32f09d432b3e202cf875e8e07dea923b120e1
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-08-01 08:48:11 -0300

    Create 20220801_installAllSolAllWebApps.ps1

---
+++ 20220801_installAllSolAllWebApps.ps1
@@ -0,0 +1,9 @@
+Add-PSSnapin Microsoft.SharePoint.PowerShell
+$webapps=get-spwebapplication;
+foreach($webapp in $webapps){
+$cont=read-host "Executando para ",$webapp,"continuar?(s/n)"
+if ($cont -eq "s") {
+Get-SPSolution | ForEach-Object {If ($_.ContainsWebApplicationResource -eq $False) {
+Install-SPSolution -Identity $_ -GACDeployment -Force} else {Install-SPSolution -Identity $_ -WebApplication $webapp -GACDeployment -Force}}
+}
+}

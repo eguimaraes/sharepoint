eb3c36156e565330b60f904b1f758b0f71b4be6e
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-08-26 00:59:58 -0300

    Create installSPOPowerShell.ps1

---
+++ installSPOPowerShell.ps1
@@ -0,0 +1,3 @@
+Get-Module -Name Microsoft.Online.SharePoint.PowerShell -ListAvailable | Select Name,Version
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+Update-Module -Name Microsoft.Online.SharePoint.PowerShell

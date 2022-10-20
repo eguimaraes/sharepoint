c2353af44a97e0becc39cd3a9038a54b78249f72
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-10-20 08:15:22 -0300

    Create criaCertificado.ps1

---
+++ criaCertificado.ps1
@@ -0,0 +1,2 @@
+makecert -n "CN=%ComputerName%, CN=localhost" -ss MY -sr LocalMachine -b 20/10/2022 -e 21/10/2032 -a sha256 -sky exchange -r -pe ekis.cer
+makecert -n "CN=%ComputerName%, CN=localhost" -ss MY -sr LocalMachine -b 20/10/2022 -e 21/10/2032 -a sha256 -sky exchange -r -pe ekis.cer

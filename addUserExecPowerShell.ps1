02d4f53ad9d1dda294cdee0d70fc5aa9ac9ea924
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-11-17 11:16:22 -0300

    Create addUserExecPowerShell.ps1
    https://learn.microsoft.com/en-us/powershell/module/sharepoint-server/get-spshelladmin?view=sharepoint-server-ps
    https://gunnarpeipman.com/sharepoint-farm-administrator-have-no-permissions-to-run-powershell-commands/
    https://learn.microsoft.com/en-us/powershell/module/sharepoint-server/add-spshelladmin?view=sharepoint-server-ps

---
+++ addUserExecPowerShell.ps1
@@ -0,0 +1,8 @@
+Get-SPShellAdmin
+Add-SPShellAdmin –username "LoginName"
+Get-SPShellAdmin
+
+
+#https://learn.microsoft.com/en-us/powershell/module/sharepoint-server/get-spshelladmin?view=sharepoint-server-ps
+#https://gunnarpeipman.com/sharepoint-farm-administrator-have-no-permissions-to-run-powershell-commands/
+#https://learn.microsoft.com/en-us/powershell/module/sharepoint-server/add-spshelladmin?view=sharepoint-server-ps

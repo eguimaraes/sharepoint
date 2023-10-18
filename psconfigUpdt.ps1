8912c191ce386f14f3513e822b86b70cc62ccb4f
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2023-10-18 05:20:07 -0300

    Create psconfigUpdt.ps1

---
+++ psconfigUpdt.ps1
@@ -0,0 +1,1 @@
+psconfig.exe -cmd helpcollections -installall -cmd secureresources -cmd services -install -cmd installfeatures -cmd applicationcontent -install -cmd upgrade -inplace b2b -force -wait

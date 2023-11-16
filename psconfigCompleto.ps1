7caf6e94060aab0d4e4c79da8253c20a31692469
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2023-11-16 11:19:42 -0300

    Update psconfigCompleto.ps1
    adicionado Install-SPApplicationContent

--- psconfigCompleto.ps1
+++ psconfigCompleto.ps1
@@ -1,2 +1,4 @@
 #original em https://blog.stefan-gossner.com/2015/08/20/why-i-prefer-psconfigui-exe-over-psconfig-exe/
+#https://learn.microsoft.com/en-us/powershell/module/sharepoint-server/install-spapplicationcontent?view=sharepoint-server-ps
 PSConfig.exe -cmd upgrade -inplace b2b -wait -cmd applicationcontent -install -cmd installfeatures -cmd secureresources -cmd services -install
+Install-SPApplicationContent

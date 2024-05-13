0bec81afe21e95cd5da1da10703e350ab74b0210
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2024-05-13 12:13:57 -0300

    Create instalarPnPPowerShell.ps1
    Comandos de instalação do PnP powershell este módulo é necessario para o SharePointOnline

---
+++ instalarPnPPowerShell.ps1
@@ -0,0 +1,4 @@
+#https://pnp.github.io/powershell/articles/installation.html
+Install-Module PnP.PowerShell -Scope CurrentUser
+Update-Module PnP.PowerShell -Scope CurrentUser
+Uninstall-Module PnP.PowerShell -AllVersions

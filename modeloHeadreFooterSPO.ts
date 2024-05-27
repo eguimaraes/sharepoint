f6240db9e9b49ffc6495ffe47e6ef74a3e5ad991
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2024-05-27 15:42:14 -0300

    Create modeloHeadreFooterSPO.ts
    https://learn.microsoft.com/en-us/sharepoint/dev/spfx/extensions/get-started/using-page-placeholder-with-extensions

---
+++ modeloHeadreFooterSPO.ts
@@ -0,0 +1,130 @@
+#https://learn.microsoft.com/en-us/sharepoint/dev/spfx/extensions/get-started/using-page-placeholder-with-extensions
+
+import { Log } from '@microsoft/sp-core-library';
+
+import {
+  BaseApplicationCustomizer,
+  PlaceholderContent,
+  PlaceholderName
+} from '@microsoft/sp-application-base';
+
+
+import * as strings from 'HeaderFooterApplicationCustomizerStrings';
+
+import styles from './AppCustomizer.module.scss';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+const LOG_SOURCE: string = 'HeaderFooterApplicationCustomizer';
+
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface IHeaderFooterApplicationCustomizerProperties {
+  // This is an example; replace with your own property
+  testMessage: string;
+  Top: string;
+  Bottom: string;
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class HeaderFooterApplicationCustomizer
+  extends BaseApplicationCustomizer<IHeaderFooterApplicationCustomizerProperties> {
+
+  private _topPlaceholder: PlaceholderContent | undefined;
+  private _bottomPlaceholder: PlaceholderContent | undefined;
+
+  
+  public onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+  
+    // Wait for the placeholders to be created (or handle them being changed) and then
+    // render.
+    this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
+  
+    return Promise.resolve();
+  }
+
+  private _renderPlaceHolders(): void {
+    console.log("HelloWorldApplicationCustomizer._renderPlaceHolders()");
+    console.log(
+      "Available placeholders: ",
+      this.context.placeholderProvider.placeholderNames
+        .map(name => PlaceholderName[name])
+        .join(", ")
+    );
+  
+    // Handling the top placeholder
+    if (!this._topPlaceholder) {
+      this._topPlaceholder = this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Top,
+        { onDispose: this._onDispose }
+      );
+  
+      // The extension should not assume that the expected placeholder is available.
+      if (!this._topPlaceholder) {
+        console.error("The expected placeholder (Top) was not found.");
+        return;
+      }
+  
+      if (this.properties) {
+        let topString: string = this.properties.Top;
+        if (!topString) {
+          topString = "(Top property was not defined.)";
+        }
+  
+        if (this._topPlaceholder.domElement) {
+          this._topPlaceholder.domElement.innerHTML = `
+          <div class="${styles.app}">
+            <div class="${styles.top}">
+              <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> ${escape(
+                topString
+              )}
+            </div>
+          </div>`;
+        }
+      }
+    }
+  
+    // Handling the bottom placeholder
+    if (!this._bottomPlaceholder) {
+      this._bottomPlaceholder = this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Bottom,
+        { onDispose: this._onDispose }
+      );
+  
+      // The extension should not assume that the expected placeholder is available.
+      if (!this._bottomPlaceholder) {
+        console.error("The expected placeholder (Bottom) was not found.");
+        return;
+      }
+  
+      if (this.properties) {
+        let bottomString: string = this.properties.Bottom;
+        if (!bottomString) {
+          bottomString = "(Bottom property was not defined.)";
+        }
+  
+        if (this._bottomPlaceholder.domElement) {
+          this._bottomPlaceholder.domElement.innerHTML = `
+          <div class="${styles.app}">
+            <div class="${styles.bottom}">
+              <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> ${escape(
+                bottomString
+              )}
+            </div>
+          </div>`;
+        }
+      }
+    }
+  }
+
+  private _onDispose(): void {
+    console.log('[HelloWorldApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.');
+  }
+
+
+}
+
+

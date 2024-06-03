a96c8496bcd0c1c8eb3e59ae2308754dacb42de6
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2024-06-03 09:22:09 -0300

    Create addFuncBTNSPFX.ts

---
+++ addFuncBTNSPFX.ts
@@ -0,0 +1,16 @@
+public render(): void {
+
+    this.domElement.innerHTML = `
+    <button id="btnCriaChecklist" type="button" class="btn btn-success">Criar dasda</button>`;
+
+    
+    
+    this.domElement.querySelector("#btnCriaChecklist")?.addEventListener('click',this.func);    }
+  
+
+
+
+
+  }
+
+ 

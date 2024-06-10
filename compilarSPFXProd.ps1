316b632bfa0cdd3739a0d1aba5ab9f82bddec148
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2024-06-10 08:08:31 -0300

    Create compilarSPFXProd.ps1
    seuqencia de comandos para liberar um pacote atualizado spfxpckg

---
+++ compilarSPFXProd.ps1
@@ -0,0 +1,8 @@
+gulp build    
+gulp bundle--production
+gulp bundle
+gulp bundle--production
+gulp bundle --ship
+gulp package-solution
+gulp package-solution --production
+gulp package-solution --ship

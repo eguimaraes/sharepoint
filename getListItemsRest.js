314c441c92fd9329dbc997e31828d05efe6a3ffb
Edvaldo Guimaraes <edvaldoguimaraes2013@hotmail.com>
2024-10-03 22:02:01 -0300

    savewsps

--- getListItemsRest.js
+++ getListItemsRest.js
@@ -1,13 +1,32 @@
-function getListItems(listName, siteurl, success, failure) {
-    $.ajax({
-        url: siteurl + "/_api/web/lists/getbytitle('" + listName + "')/items",
-        method: "GET",
-        headers: { "Accept": "application/json; odata=verbose" },
-        success: function (data) {
-            success(data);
-        },
-        error: function (data) {
-            failure(data);
-        }
-    });
-}
+function retriveListItem()  
+{  
+  
+    $.ajax  
+    ({  
+        url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/GetByTitle('companyInfo')/items?$select=Company,Industry",  
+        type: type,  
+        data: data,  
+        headers:  
+        {  
+            "Accept": "application/json;odata=verbose",  
+            "Content-Type": "application/json;odata=verbose",  
+            "X-RequestDigest": $("#__REQUESTDIGEST").val(),  
+            "IF-MATCH": "*",  
+            "X-HTTP-Method": null  
+        },  
+        cache: false,  
+        success: function(data)   
+        {  
+            $("#ResultDiv").empty();  
+            for (var i = 0; i < data.d.results.length; i++)   
+            {  
+                var item = data.d.results[i];  
+                $("#ResultDiv").append(item.Company + "\t" + item.Industry + "<br/>");  
+            }  
+        },  
+        error: function(data)  
+        {  
+            $("#ResultDiv").empty().text(data.responseJSON.error);  
+        }  
+    });  
+}  
